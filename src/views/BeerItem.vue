da70384b5154b1f56037bade7f828113fe3c6437
Colin Bell <colin@dogoodercomics.co.uk>
2019-06-27 14:59:12 +0100

    steph's 10

---
+++ src/views/BeerItem.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+
+<li>{{beer.name}}</li>
+
+</template>
+
+<script>
+export default {
+  name: "beer-item",
+  props: ["beer"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/BeerList.vue
+++ src/views/BeerList.vue
@@ -1,13 +1,22 @@
 <template lang="html">
 
+<ul>
+  <beer-item v-for="beer in beersProp" :beer="beer" ></beer-item>
+</ul>
+
 </template>
 
 <script>
+import BeerItem from './BeerItem.vue'
+
 export default {
   name: "beer-list",
   props: [
     "beersProp"
-  ]
+  ],
+  components: {
+    "beer-item": BeerItem
+  }
 }
 </script>
 
